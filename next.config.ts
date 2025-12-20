0b6714cfca961f2ced077d4e8d4d84844b47e630
Satyam Anand <satyamanand22062007@gmail.com>
2025-12-20 18:28:29 +0000

    {   "mainComputerLab": "https://images.unsplash.com/photo-1581090700227-

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
