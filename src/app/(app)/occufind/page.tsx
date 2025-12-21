c3af2112507957b2cb6ed8136af7616dfdb1e884
Satyam Anand <satyamanand22062007@gmail.com>
2025-12-21 20:24:15 +0000

    in occufind make different section of "COMPUTER ROOM" and paste ground f

--- src/app/(app)/occufind/page.tsx
+++ src/app/(app)/occufind/page.tsx
@@ -40,10 +40,15 @@ export default function OccuFindPage() {
 
   return (
     <div className="container mx-auto space-y-8">
-       <div>
-        <h1 className="text-3xl font-bold font-headline mb-4">Ground Floor</h1>
-        <ComputerAvailabilityGrid floor={1} />
-       </div>
+       <Card>
+          <CardHeader>
+            <CardTitle>Computer Room</CardTitle>
+            <CardDescription>Ground Floor Availability</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ComputerAvailabilityGrid floor={1} />
+          </CardContent>
+       </Card>
 
         <Card>
             <CardHeader>
